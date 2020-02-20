2d79a85833068e8176b79e5fab26205a0946ee86
Judson de Almeida Rodrigues <judson.rodrigues@geap.com.br>
2020-02-20 15:22:42 -0300

    Componentes com comunicação

---
+++ src/components/FilmesLista.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="row">
+		<!-- coluna 1 -->
+		<div class="col-8">
+			<h2>Filmes</h2>
+
+			<ul class="list-group list-group-flush">
+				<filmes-lista-iten
+					v-for="filme in filmes"
+					:key="filme.id"
+					:filme="filme"
+					:class="aplicarClasseActive(filme)"
+					@selecionarFilme="filmeSelecionado = $event"
+				/>
+			</ul>
+		</div>
+		<!-- coluna 2 -->
+		<div class="col-4">
+			<FilmesListaItenInfo />
+		</div>
+	</div>
+</template>
+
+<script>
+import FilmesListaIten from "./FilmesListaIten.vue";
+import FilmesListaItenInfo from "./FilmesListaItenInfo.vue";
+
+export default {
+	components: {
+		FilmesListaIten,
+		FilmesListaItenInfo
+	},
+	data() {
+		return {
+			filmes: [
+				{
+					id: 1,
+					titulo: "Vingadores: Guerra Infinita",
+					ano: 2018,
+					diretor: "Stan Lee"
+				},
+				{ id: 2, titulo: "Pantera Negra", ano: 2018, diretor: "Stan Lee" },
+				{
+					id: 3,
+					titulo: "Homem Formiga e Vespa",
+					ano: 2018,
+					diretor: "Stan Lee"
+				},
+				{ id: 4, titulo: "Deadpool", ano: 2018, diretor: "Stan Lee" },
+				{ id: 5, titulo: "Superman DC", ano: 2018, diretor: "Stan Lee" }
+			],
+			filmeSelecionado: undefined
+		};
+	},
+
+	methods: {
+		aplicarClasseActive(filmeIterado) {
+			return {
+				active:
+					this.filmeSelecionado && this.filmeSelecionado.id === filmeIterado.id
+			};
+		}
+	}
+};
+</script>
---
+++ src/components/FilmesListaIten.vue
@@ -0,0 +1,26 @@
+<template>
+	<!-- <div>
+		<input type="text" :value="titulo" :placeholder="$attrs.placeholder" />
+	</div>-->
+	<li class="list-group-item">
+		<span>{{filme.titulo}} | {{filme.ano}}</span>
+		<button @click="selecionar()" class="btn btn-secondary float-right">Selecionar</button>
+	</li>
+</template>
+
+<script>
+export default {
+	props: {
+		filme: {
+			type: Object,
+			required: true
+		}
+	},
+	methods: {
+		selecionar: function() {
+			console.log(this.filme.titulo);
+			this.$emit("selecionarFilme", this.filme);
+		}
+	}
+};
+</script>
